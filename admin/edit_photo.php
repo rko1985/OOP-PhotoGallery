e3538216c16460d7b02ec30094deb54d438eafde
rko1985 <35703264+rko1985@users.noreply.github.com>
2018-10-15 09:00:30 -1000

    lesson 123 finished

--- admin/edit_photo.php
+++ admin/edit_photo.php
@@ -4,8 +4,9 @@
 
 <?php
 
-
-
+if(isset($_POST['update'])){
+    echo "yes it works";
+}
 // $photos = Photo::find_all();
 
 ?>
@@ -32,12 +33,14 @@
 
               <!-- Page Heading -->
               <div class="row">
-                  <div class="col-lg-8">
+                  <div class="col-lg-12">
                       <h1 class="page-header">
                           Photos
                           <small>Subheading</small>
                       </h1>
-                      <div class="col-md-12">
+
+                    <form action="edit_photo.php" method="post">
+                      <div class="col-md-8">
 
                         <div class="form-group">
                             <input type="text" name="title" class="form-control">
@@ -54,9 +57,47 @@
                             <label for="caption">Description</label>
                             <textarea class="form-control" name="description" id="" cols="30" rows="10"></textarea>
                         </div>
-                                         
+                                                                 
                       </div>
+
+                      <div class="col-md-4" >
+                            <div  class="photo-info-box">
+                                <div class="info-box-header">
+                                   <h4>Save <span id="toggle" class="glyphicon glyphicon-menu-up pull-right"></span></h4>
+                                </div>
+                            <div class="inside">
+                              <div class="box-inner">
+                                 <p class="text">
+                                   <span class="glyphicon glyphicon-calendar"></span> Uploaded on: April 22, 2030 @ 5:26
+                                  </p>
+                                  <p class="text ">
+                                    Photo Id: <span class="data photo_id_box">34</span>
+                                  </p>
+                                  <p class="text">
+                                    Filename: <span class="data">image.jpg</span>
+                                  </p>
+                                 <p class="text">
+                                  File Type: <span class="data">JPG</span>
+                                 </p>
+                                 <p class="text">
+                                   File Size: <span class="data">3245345</span>
+                                 </p>
+                              </div>
+                              <div class="info-box-footer clearfix">
+                                <div class="info-box-delete pull-left">
+                                    <a  href="delete_photo.php?id=<?php echo $photo->id; ?>" class="btn btn-danger btn-lg ">Delete</a>   
+                                </div>
+                                <div class="info-box-update pull-right ">
+                                    <input type="submit" name="update" value="Update" class="btn btn-primary btn-lg ">
+                                </div>   
+                              </div>
+                            </div>          
+                        </div>
+                    </div>
+
+                    </form>
                   </div>
+                  
               </div>
               <!-- /.row -->
 
