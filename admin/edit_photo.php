4f2d732adcea242e7e8cbdeca93063a80ccd3434
rko1985 <35703264+rko1985@users.noreply.github.com>
2018-10-16 07:38:50 -1000

    lesson 130 finished

--- admin/edit_photo.php
+++ admin/edit_photo.php
@@ -67,6 +67,11 @@ if(empty($_GET['id'])){
                         <div class="form-group">
                             <input type="text" name="title" class="form-control" value="<?php echo $photo->title;?>">
                         </div>
+
+                        <div class="form-group">
+                            <a class="thumbnail" href="#"><img src="<?php echo $photo->picture_path();?>" alt=""></a>
+                        </div>
+
                         <div class="form-group">
                             <label for="caption">Caption</label>
                             <input type="text" name="caption" class="form-control" value="<?php echo $photo->caption;?>">
--- admin/photos.php
+++ admin/photos.php
@@ -51,7 +51,7 @@ $photos = Photo::find_all();
                                     <?php  foreach($photos as $photo) : ?>
                                     <tr>
                                         <td>
-                                            <img src="<?php echo $photo->picture_path(); ?>" alt="">
+                                            <img class="admin-photo-thumbnail" src="<?php echo $photo->picture_path(); ?>" alt="">
                                             <div class="pictures_link">
                                                 <a href="delete_photo.php?id=<?php echo $photo->id; ?>">Delete</a>
                                                 <a href="edit_photo.php?id=<?php echo $photo->id; ?>">Edit</a>
