3f4e7916f3fc4649ce8465d54b6044976181f8d1
rko1985 <35703264+rko1985@users.noreply.github.com>
2018-10-16 07:13:38 -1000

    lesson 127 finished

--- admin/edit_photo.php
+++ admin/edit_photo.php
@@ -21,6 +21,8 @@ if(empty($_GET['id'])){
            $photo->alternate_text = $_POST['alternate_text'];
            $photo->description = $_POST['description'];
 
+           $photo->save();
+
         }
 
     }
@@ -59,23 +61,24 @@ if(empty($_GET['id'])){
                           <small>Subheading</small>
                       </h1>
 
-                    <form action="edit_photo.php" method="post">
+                    <form action="" method="post">
                       <div class="col-md-8">
 
                         <div class="form-group">
-                            <input type="text" name="title" class="form-control">
+                            <input type="text" name="title" class="form-control" value="<?php echo $photo->title;?>">
                         </div>
                         <div class="form-group">
                             <label for="caption">Caption</label>
-                            <input type="text" name="caption" class="form-control" value="<?php echo $photo->title;?>">
+                            <input type="text" name="caption" class="form-control" value="<?php echo $photo->caption;?>">
                         </div>
                         <div class="form-group">
                             <label for="caption">Alternate Text</label>
-                            <input type="text" name="alternate_text" class="form-control">
+                            <input type="text" name="alternate_text" class="form-control" value="<?php echo $photo->alternate_text; ?>">
                         </div>
                         <div class="form-group">
                             <label for="caption">Description</label>
-                            <textarea class="form-control" name="description" id="" cols="30" rows="10"></textarea>
+                            <textarea class="form-control" name="description" id="" cols="30" rows="10"><?php echo $photo->description; ?></textarea>
+                            
                         </div>
                                                                  
                       </div>
