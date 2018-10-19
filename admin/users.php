315b796af861b8570879805aab3cda13bcbdeae5
rko1985 <35703264+rko1985@users.noreply.github.com>
2018-10-19 10:17:19 -1000

    lesson 135 finished

--- admin/users.php
+++ admin/users.php
@@ -1,4 +1,13 @@
 <?php include("includes/header.php"); ?>
+<?php if(!$session->is_signed_in()){ redirect("login.php"); } ?>
+
+
+<?php 
+
+$users = User::find_all();
+
+?>
+
 
         <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -23,17 +32,40 @@
               <div class="row">
                   <div class="col-lg-12">
                       <h1 class="page-header">
-                          USERS
+                          users
                           <small>Subheading</small>
                       </h1>
-                      <ol class="breadcrumb">
-                          <li>
-                              <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                          </li>
-                          <li class="active">
-                              <i class="fa fa-file"></i> Blank Page
-                          </li>
-                      </ol>
+                      <div class="col-md-12">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Photo</th>
+                                        <th>Username</th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php  foreach($users as $user) : ?>
+                                        <tr>
+                                            <td><?php echo $user->id; ?></td>
+                                            <td><img class="admin-user-thumbnail user_image" src="<?php echo $user->image_path_and_placeholder(); ?>" alt=""></td>
+                                            <td>
+                                                <?php echo $user->username; ?>
+                                                <div class="action_links">
+                                                    <a href="delete_user.php?id=<?php echo $user->id; ?>">Delete</a>
+                                                    <a href="edit_user.php?id=<?php echo $user->id; ?>">Edit</a>
+                                                    <a href="#">View</a>
+                                                </div>
+                                            </td>
+                                            <td><?php echo $user->first_name; ?></td>
+                                            <td><?php echo $user->last_name; ?></td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                </tbody>                            
+                            </table> <!--End of Table -->                     
+                      </div>
                   </div>
               </div>
               <!-- /.row -->
