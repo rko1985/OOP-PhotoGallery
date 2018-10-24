6fa72e81b546f6e0620906dd26ca7e8bfffb66a1
rko1985 <35703264+rko1985@users.noreply.github.com>
2018-10-24 08:58:45 -1000

    displaying and making comments frontend lesson 155 complete

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -11,6 +11,7 @@ require_once("database.php");
 require_once("db_object.php");
 require_once("user.php");
 require_once("photo.php");
+require_once("comment.php");
 require_once("session.php");
 
  ?>
